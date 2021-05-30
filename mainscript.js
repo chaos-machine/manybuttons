c677211ff2ad47087793ac558f2b350c8a6dc9e0
Drayfus7033 <tudor703344@gmail.com>
2021-05-30 20:52:43 +0200

    ePlease enter the commit message for your changes. Lines starting t removed filter :wq q :wqeda :wq

--- mainscript.js
+++ mainscript.js
@@ -1,55 +1,64 @@
-﻿//filtering dom elements based on
-
-//brand: youtube, facebook, instagram, uber, etc.
-//device: desktop, iPad, phone
-//channel: app
-//mode:light, dark
-//type: icon, no icon, text, ghost, filled
-//decoration: icon, no icon
-
-//html id="youtube phone app light icon filled"
-// variable "to check"
-//filter checkbox adds to this variable "to check"
-//press an Apply Button
-//check all <p> in DOM.
-//if a <p> id == "to check" --> show the <p>
-//if a <p> id != "to check" --> hide the <p>
-
-
-var allIDs
+﻿var allIDs
 var filter = ""
-var x = -1
+var filterArray=[]
 
-console.log("hello")
 
 $(document).ready(function () {
     allIDs = document.querySelectorAll('*[id]:not([id=""])')
- 
+    // console.log(allIDs)
+
     $('.checkbox-label').change(function () {
         //make sure there is an id set to the checkbox
-        if (this.id != "undefined")
-        {
+        if (this.id != "undefined") {
             if (filter != this.id && !filter.includes(this.id))
-                filter += this.id
-            else
+                filter += "-" + this.id
+            else {
                 filter = filter.replace(this.id, "")
+                filter = filter.replace("-", "")
+            }
 
-            console.log(filter);
         }
+        filterArray = filter.split("-")
+
+        var newArray = filterArray.filter(function (v) { return v !== '' });
+        filterArray = newArray
+
+        console.log("Filteres are: " + filterArray);
+
+
     });
 
     $('#btnFilter').click(function () {
-        for (var i = 0; i < Object.keys(allIDs).length; i++)
-            {
-                if (allIDs[i].id.includes(filter))
-                    console.log("There ")
-            }
 
-        })
+       
+        for (var i = 0; i < Object.keys(allIDs).length; i++)
+        {
+       
+            for (var k = 0; k < Object.keys(filterArray).length; k++)
+            {
+                //if a picture matches the filter, show it
+                if (allIDs[i].id.includes(filterArray[k]) && allIDs[i].className == "col")
+                {
+                    console.log("Elements matching: " + allIDs[i].id)
+                    $("#" + allIDs[i].id).show()
+                }
+
+                //if a picture doesn't match the filter, hide it
+                if (!allIDs[i].id.includes(filterArray[k]) && allIDs[i].className == "col") {
+                    console.log("Elements NOT matching: " + allIDs[i].id)
+                    $("#" + allIDs[i].id).hide()
+                }
+            }
+        }
+        
+
+    })
 });
 
 
 
 
 
-
+//add a filter -> go through all pictures and hide or show them
+//remove the filter -> filter is " " -> go through all pictures, nothing will match, so all will be shown
+//
